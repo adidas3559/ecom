e3db72b9e6b092e103b60b41f223ba6348ec988b
adidas3559 <forbes3559@gmail.com>
2020-10-05 22:47:59 -0500

    trying to push from A Dog 2

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import ShopSearchBar from './shopSearchBar';
+import ShopProduct from './shopProduct';
 
 class Shop extends Component {
 
@@ -14,16 +16,37 @@ class Shop extends Component {
             }
         ]
         this.props.setHeaderLinks(headerLinks);
-        // fetch shop products action creator
-        // fetch navbar links
-            // set navbar links
-            // filter products with links
+        this.props.fetchShopCategories();
+
+        // filter products with links
+        this.props.fetchShopProducts();
     }
+
+    shouldComponentUpdate(nextProps) {
+        if(this.props != nextProps) {
+            this.props.setNavbarLinks(nextProps.categories, (_id) => this.props.filterProductsWithCategoryId(_id));
+        }
+        return true
+    }
+
+    onSubmit = (fields) => {
+        this.props.filterProductsWithQuery(fields)
+    }
+
     render() {
+
         return (
             <div className='shop'>
-                {/* shop search bar */}
-                {/* shop product */}
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
+                <div className='shop__products'>
+                    {
+                        this.props.filteredProducts.map(product => {
+                            return (
+                                <ShopProduct {...product} key={product._id} />
+                            )
+                        })
+                    }
+                </div>
                 {/* shop cart button */}
             </div>
         )
@@ -31,7 +54,11 @@ class Shop extends Component {
 }
 
 function mapStateToProps(state) {
-    return { state } 
+    const { categories, filteredProducts } = state.shop;
+    return {
+        categories,
+        filteredProducts
+    } 
 }
 
 Shop = connect(mapStateToProps, actions)(Shop);
